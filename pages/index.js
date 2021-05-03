0fbc921446b8b093a46eaf7a1f936d750e236e23
Jonah Satya Anand <jsatyaanand@equinix.com>
2021-05-03 11:40:29 +0530

    Using getStaticProps method to fetch events.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '@/components/Layout';
-
-export default function Home() {
+import {API_URL} from '@/config/index';
+export default function Home({events}) {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {events},
+    revalidate: 1
+  }
 }
