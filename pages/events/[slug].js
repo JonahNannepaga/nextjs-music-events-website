8240675124fd02b1e6d8a924d82d0b9da67010f9
Jonah Satya Anand <jsatyaanand@equinix.com>
2021-05-04 11:25:53 +0530

    Basic Frontend app development done.(routing)

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,52 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
 import Layout from '@/components/Layout';
 import {API_URL} from '@/config/index';
+import styles from '@/styles/Event.module.css';
 export default function EventPage({evt}) {
+    const deleteEvent = () => {
+        console.log('DELETE');
+    };
     return (
         <Layout>
-            <h1>{evt.name}</h1>
+            <div className={styles.event}>
+                <div className={styles.controls}>
+                    <Link href={`/events/edit/${evt.id}`}>
+                        <a><FaPencilAlt/> Edit Event</a>
+                    </Link>
+                    <a href="#" className={styles.delete} onClick={deleteEvent}>
+                        <FaTimes/> Delete Event
+                    </a>
+                </div>
+
+                <span>
+                    {evt.date} at {evt.time}
+                </span>
+
+                <h1>{evt.name}</h1>
+
+                {evt.image && (
+                    <div className={styles.image}>
+                        <Image src={evt.image} width={960}
+                        height={600}/>
+                    </div>
+                )}
+
+                <h3>Performers:</h3>
+                <p>{evt.performers}</p>
+                <h3>Description:</h3>
+                <p>{evt.description}</p>
+                <h3>Venue: {evt.venue}</h3>
+                <p>{evt.address}</p>
+
+                <Link href='/events'>
+                    <a className={styles.back}>
+                       {'<'} Go back
+                    </a>
+                </Link>
+
+            </div>
         </Layout>
     )
 }
