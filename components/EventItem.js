ec5bf3f510968a1ec4f1dea75c5291f84c820d3e
Jonah Nannepaga <jonahnannepaga@Jonahs-MacBook-Pro.local>
2021-05-04 12:34:05 +0530

    Strapi backend integration changes.

--- components/EventItem.js
+++ components/EventItem.js
@@ -6,13 +6,13 @@ export default function EventItem({evt}) {
     return (
         <div className={styles.event}>
             <div className={styles.img}>
-                <Image src={evt.image ? evt.image : '/images/event-default.png'} width={170} height={100} />
+                <Image src={evt.image ? evt.image.formats.thumbnail.url : '/images/event-default.png'} width={170} height={100} />
             </div>
 
             <div className={styles.info}>
                 <span>
-                {/*new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}*/}
-                {evt.date} at {evt.time}
+                {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
+                {/* {evt.date} at {evt.time} */}
                 </span>
                 <h3>{evt.name}</h3>
             </div>
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -21,14 +21,14 @@ export default function EventPage({evt}) {
                 </div>
 
                 <span>
-                    {evt.date} at {evt.time}
+                    {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
                 </span>
 
                 <h1>{evt.name}</h1>
 
                 {evt.image && (
                     <div className={styles.image}>
-                        <Image src={evt.image} width={960}
+                        <Image src={evt.image.formats.medium.url} width={960}
                         height={600}/>
                     </div>
                 )}
@@ -52,7 +52,7 @@ export default function EventPage({evt}) {
 }
 
 export async function getStaticPaths() {
-    const res = await fetch(`${API_URL}/api/events`);
+    const res = await fetch(`${API_URL}/events`);
     const events = await res.json();
     const paths = events.map(evt => ({
         params: {slug: evt.slug}
@@ -64,11 +64,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({params:{slug}}) {
-    const res = await fetch(`${API_URL}/api/events/${slug}`);
+    const res = await fetch(`${API_URL}/events?slug=${slug}`);
     const events = await res.json();
     return {
         props: {
-            evt: events[0]
+            evt: events[0],
         },
         revalidate: 1
     }
