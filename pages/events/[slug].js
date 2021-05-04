17b92c0acf225354c41c3c060ce5871b52ce23f3
Jonah Satya Anand <jsatyaanand@equinix.com>
2021-05-04 11:05:16 +0530

    getStaticPath to fetch the slugs/id for event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,42 @@
-import {useRouter} from 'next/router'
 import Layout from '@/components/Layout';
-export default function EventPage() {
-    const router = useRouter();
+import {API_URL} from '@/config/index';
+export default function EventPage({evt}) {
     return (
         <Layout>
-            <h1>My Event</h1>
-            <h3>{router.query.slug}</h3>
+            <h1>{evt.name}</h1>
         </Layout>
     )
 }
+
+export async function getStaticPaths() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+    const paths = events.map(evt => ({
+        params: {slug: evt.slug}
+    }));
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+export async function getStaticProps({params:{slug}}) {
+    const res = await fetch(`${API_URL}/api/events/${slug}`);
+    const events = await res.json();
+    return {
+        props: {
+            evt: events[0]
+        },
+        revalidate: 1
+    }
+}
+
+// export async function getServerSideProps({query:{slug}}) {
+//     const res = await fetch(`${API_URL}/api/events/${slug}`);
+//     const events = await res.json();
+//     return {
+//         props: {
+//             evt: events[0]
+//         },
+//     }
+// }
